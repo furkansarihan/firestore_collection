2d7a004ad65280834c0c6bb66824d77fa9c3d52f
Furkan Sarıhan <sarihan3726@gmail.com>
2020-12-13 19:29:53 +0300

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## [0.0.2]
+## [0.0.3]
 
 * Breaking Change: order related parameters moved to QueryOrder object.
 * unnecessary dependencies removed
--- lib/firestore_document.dart
+++ lib/firestore_document.dart
@@ -3,7 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 extension FirestoreDocumentExtension on DocumentReference {
   Future<DocumentSnapshot> stream() async {
     try {
-      return this.get(GetOptions(source: Source.cache));
+      DocumentSnapshot ds = await this.get(GetOptions(source: Source.cache));
+      if (ds == null) return this.get(GetOptions(source: Source.server));
+      return ds;
     } catch (_) {
       return this.get(GetOptions(source: Source.server));
     }
